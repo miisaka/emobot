71d220dbdf91e910615182d781acb249340f6d05
Steven Ha <steven_ha666@hotmail.com>
2018-01-27 16:31:33 -0500

    added register frontend

--- app/static/src/app/app.module.ts
+++ app/static/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
