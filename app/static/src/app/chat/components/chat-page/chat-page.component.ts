608a32675d0269c48eda77d5ab12a0987a5a00b0
Laurendy Lam <lamlaurendy@gmail.com>
2018-01-27 21:13:27 -0500

    add toaster dependency and animations

--- app/static/src/app/chat/components/chat-page/chat-page.component.ts
+++ app/static/src/app/chat/components/chat-page/chat-page.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'chat-page',
   template: `
-    <div class="chat-page">
-      <div class="chat-box">
-        <div class="text-center">
-          <h1>Chat</h1>
-        </div>
-        <chat class="chat"></chat>
+    <div class="chat-box">
+      <div class="text-center">
+        <h1>Chat</h1>
       </div>
+      <chat class="chat"></chat>
     </div>
   `,
   styleUrls: ['./chat-page.component.scss']
